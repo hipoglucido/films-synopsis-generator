5c253d366da0f0b43712ef62b31476308f18da6e
Víctor García Cazorla <gvictorg@int2.cartesius.surfsara.nl>
2017-05-22 00:33:02 +0200

    changed structure

--- src/main.py
+++ src/main.py
@@ -6,17 +6,18 @@ import re
 import numpy as np
 
 from keras.models import Sequential
-from keras.layers import LSTM, Embedding, TimeDistributed, Dense, RepeatVector, Merge, Activation, Flatten
+from keras.layers import LSTM, Embedding, TimeDistributed, Dense, RepeatVector, Activation, Flatten, Merge
+#from keras.layers.merge import Concatenate
 from keras.preprocessing import sequence
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import ModelCheckpoint, TensorBoard
 
 #import cPickle as pickle
 from sklearn.preprocessing import MultiLabelBinarizer
-
+import tensorflow as tf
 
 
 EMBEDDING_DIM = 128
-
+MAX_CAP_LEN = 150
 
 class CaptionGenerator():
 
@@ -30,16 +31,16 @@ class CaptionGenerator():
         self.variable_initializer()
         
     def tokenize(self,s):
-        return ' '.join(re.findall(r"[\w]+|[^\s\w]", s))
+        return ' '.join(re.findall(r"[\w]+|[^\s\w]", s)).lower()
         
     def variable_initializer(self):
-        self.df = pd.read_csv(filepath_or_buffer  = os.path.join(settings.RAW_DATA_DIR,'synopsis_genres.csv'),sep = '#',encoding = 'latin_1',index_col = 'ID',nrows = 50)
-        df = self.df
+        df = pd.read_csv(filepath_or_buffer  = os.path.join(settings.DATA_DIR,'synopsis_genres.csv'),sep = '#',encoding = 'latin_1',index_col = 'ID')
+        df = df[df['Synopsis'].notnull() & df['Genre'].notnull()]
         df.info()
-        print(df.head())
-        nb_samples = df.shape[0]
+        print(str(df.head()).encode('latin1'))
+        #nb_samples = df.shape[0]
         self.caps = list(df['Synopsis'].map(self.tokenize).values)
-        print(self.caps)
+        #print(self.caps)
         caps = self.caps
         self.total_samples=0
         for text in caps:
@@ -52,6 +53,7 @@ class CaptionGenerator():
             unique.extend(word)
 
         unique = list(set(unique))
+        #print(str(unique).encode('latin1'))
         self.vocab_size = len(unique)
         self.word_index = {}
         self.index_word = {}
@@ -63,15 +65,15 @@ class CaptionGenerator():
         for caption in caps:
             if(len(caption.split()) > max_len):
                 max_len = len(caption.split())
-        self.max_cap_len = max_len
+        #self.max_cap_len = max_len
+        self.max_cap_len = MAX_CAP_LEN
         print("Vocabulary size: "+str(self.vocab_size))
         print("Maximum caption length: "+str(self.max_cap_len))
         print("Variables initialization done!")
         print("Processing genres...")
         self.mlb = MultiLabelBinarizer()
         self.genres_features = self.mlb.fit_transform(df['Genre'].map(lambda x: x.split('|')))
-        print(len(self.mlb.classes_)," different genres found:",str(self.mlb.classes_)[:100],"...")
-        
+        print(len(self.mlb.classes_)," different genres found:",str(self.mlb.classes_).encode('latin1')[:100],"...")
         print(self.genres_features.shape)
     def to_genre(self, x):
         return '|'.join(self.mlb.inverse_transform(x[None,:])[0])
@@ -109,38 +111,46 @@ class CaptionGenerator():
                         partial_caps = sequence.pad_sequences(partial_caps, maxlen=self.max_cap_len, padding='post')
                         total_count = 0
                         gen_count+=1
-                        print("yielding count: "+str(gen_count))
-                        print(len(images),images[0].shape,partial_caps.shape,next_words.shape)
+                        #print("yielding count: "+str(gen_count))
+                        #print(len(images),images[0].shape,partial_caps.shape,next_words.shape)
                         #print(next_words.mean())
+                        '''
+                        for i in range(batch_size):
+                            print(str(self.to_genre(images[i])).encode('latin1'))
+                            print(str(self.to_text(partial_caps[i])).encode('latin1'))
+                            print(str(self.to_text(np.nonzero(next_words[i])[0])).encode('latin1'))
+                        print("____________________________________________")
+                        '''
                         yield [[images, partial_caps], next_words]
                         partial_caps = []
                         next_words = []
                         images = []
         
-    def load_image(self, path):
-        img = image.load_img(path, target_size=(224,224))
-        x = image.img_to_array(img)
-        return np.asarray(x)
 
 
     def create_model(self, ret_model = False):
         #base_model = VGG16(weights='imagenet', include_top=False, input_shape = (224, 224, 3))
         #base_model.trainable=False
+        #with tf.device("/gpu:1"):
         image_model = Sequential()
         #image_model.add(base_model)
         #image_model.add(Flatten())
-        image_model.add(Dense(EMBEDDING_DIM, input_dim = len(self.mlb.classes_), activation='relu'))
-
+        image_model.add(Dense(units = EMBEDDING_DIM,
+                              input_dim = len(self.mlb.classes_),
+                              activation='relu'))
         image_model.add(RepeatVector(self.max_cap_len))
-
+        #with tf.device("/gpu:0"):
         lang_model = Sequential()
-        lang_model.add(Embedding(self.vocab_size, 256, input_length=self.max_cap_len))
-        lang_model.add(LSTM(256,return_sequences=True))
+        lang_model.add(Embedding(input_dim = self.vocab_size, 
+                                output_dim = 256,
+                                input_length=self.max_cap_len))
+        lang_model.add(LSTM(units = 256,
+                            return_sequences=True))
         lang_model.add(TimeDistributed(Dense(EMBEDDING_DIM)))
-
+        #with tf.device("/gpu:1"):
         model = Sequential()
         model.add(Merge([image_model, lang_model], mode='concat'))
-        model.add(LSTM(1000,return_sequences=False))
+        model.add(LSTM(units = 1000,return_sequences=False))
         model.add(Dense(self.vocab_size))
         model.add(Activation('softmax'))
 
@@ -155,11 +165,9 @@ class CaptionGenerator():
     def get_word(self,index):
         return self.index_word[index]    
 
-        
-        
-from keras.callbacks import ModelCheckpoint
 
-def train_model(weight = None, batch_size=32, epochs = 10):
+        
+def train_model(weight, batch_size, epochs):
 
     cg = CaptionGenerator()
     model = cg.create_model()
@@ -168,11 +176,20 @@ def train_model(weight = None, batch_size=32, epochs = 10):
         model.load_weights(weight)
 
     counter = 0
-    file_name = 'weights-improvement-{epoch:02d}.hdf5'
-    checkpoint = ModelCheckpoint(file_name, monitor='loss', verbose=1, save_best_only=True, mode='min')
-    callbacks_list = [checkpoint]
-    print(cg.total_samples/batch_size)
-    model.fit_generator(cg.data_generator(batch_size=batch_size), steps_per_epoch=cg.total_samples/batch_size, epochs=epochs, verbose=2, callbacks=callbacks_list)
+    file_name = os.path.join(settings.WEIGHTS_DIR,'LSTM_weights-{epoch:03d}-tloss{loss:.4f}.hdf5')
+    checkpoint = ModelCheckpoint(file_name, monitor='loss', save_best_only=True, mode='min')
+    
+    tf_logs = TensorBoard(
+                log_dir = settings.TENSORBOARD_LOGS_DIR,
+                histogram_freq = 1,
+                write_graph = True,
+                write_images = True)
+    callbacks_list = [checkpoint,tf_logs]
+    model.fit_generator(cg.data_generator(batch_size=batch_size),
+                        steps_per_epoch=500,#cg.total_samples/batch_size,
+                        epochs=epochs,
+                        workers=1,
+                        callbacks=callbacks_list)
     try:
         model.save('WholeModel.h5', overwrite=True)
         model.save_weights('Weights.h5',overwrite=True)
@@ -181,7 +198,7 @@ def train_model(weight = None, batch_size=32, epochs = 10):
     print("Training complete...\n")
 
 if __name__ == '__main__':
-    train_model(epochs=50)
+    train_model(weight = None, batch_size = 512, epochs=1000)
 '''
 c = CaptionGenerator()
 
--- src/settings.py
+++ src/settings.py
@@ -2,26 +2,18 @@
 
 import os
 
-#CONFIGURATION
-EMBEDDING_DIM = 128
 
 
-#ROOT_DIR = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), ".."))
-ROOT_DIR = '..'#'/home/vgarciacazorla/NC/films-synopsis-generator'
+
+ROOT_DIR = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), ".."))
+#ROOT_DIR = '..'#'/home/vgarciacazorla/NC/films-synopsis-generator'
 DATA_DIR = os.path.join(ROOT_DIR, "data")
 
 
-## Input directories
+## Directories
 INPUT_DIR = os.path.join(DATA_DIR, "input")
-DATASETS_DIR = os.path.join(INPUT_DIR, "datasets")
-RAW_DATA_DIR = os.path.join(DATASETS_DIR,"raw")
-TRAIN_DATA_DIR = os.path.join(DATASETS_DIR,"train")
-TEST_DATA_DIR = os.path.join(DATASETS_DIR,"test")
-INPUT_WEIGHTS_DIR = os.path.join(INPUT_DIR, "weights")
-WORD_EMBEDDINGS_PATH = os.path.join(INPUT_DIR,"SBW-vectors-300-min5.txt")
+WEIGHTS_DIR = os.path.join(DATA_DIR, "weights")
+WORD_EMBEDDINGS_PATH = os.path.join(DATA_DIR,"SBW-vectors-300-min5.txt")
+TENSORBOARD_LOGS_DIR = os.path.join(DATA_DIR, 'tensorboard_logs')
 
-##Output directories
-OUTPUT_DIR = os.path.join(DATA_DIR, "output")
-TENSORBOARD_LOGS_DIR = os.path.join(OUTPUT_DIR, 'tensorboard_logs')
-OUTPUT_WEIGHTS_DIR = os.path.join(OUTPUT_DIR, "weights")
 
