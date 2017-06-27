28f31d908102a910435c4b0bc76a1669cfea4174
hipoglucido <gcvictorgc@gmail.com>
2017-06-27 10:44:46 +0200

    Added comments

--- src/settings.py
+++ src/settings.py
@@ -24,10 +24,11 @@ TENSORBOARD_LOGS_DIR = os.path.join(DATA_DIR, 'tensorboard_logs')
 
 '''
 
-LSTM_w2v0_v20000_g10_w-005-tloss6.4035-vloss6.4556.hdf5
-LSTM_w2v1_v20000_g10_w-005-tloss6.4639-vloss6.6562.hdf5
-LSTM_w2v0_v7000_g8_w-002-tloss5.9603-vloss5.9640.hdf5
-LSTM_w2v1_v7000_g8_w-002-tloss5.1118-vloss4.8778.hdf5
+LSTM_w2v0_v20000_g10_w-007-tloss6.5609-vloss6.5036.hdf5
+LSTM_w2v0_v7000_g8_w-005-tloss5.3248-vloss5.2801.hdf5
+LSTM_w2v1_v20000_g10_w-007-tloss6.8566-vloss8.0779.hdf5
+LSTM_w2v1_v7000_g8_w-005-tloss4.4168-vloss4.3784.hdf5
+
 
 LSTM_w2v1_v50000_g25_w-003-tloss8.8982-vloss8.9871
 LSTM_w2v0_v50000_g25_w-003-tloss9.3067-vloss9.3207
@@ -35,7 +36,7 @@ LSTM_w2v0_v50000_g25_w-003-tloss9.3067-vloss9.3207
 ## TRAINING
 BATCH_SIZE = 128
 EPOCHS = 10000
-WEIGHTS_PATH = os.path.join(WEIGHTS_DIR, 'LSTM_w2v0_v50000_g25_w-003-tloss9.3067-vloss9.3207.hdf5')
+WEIGHTS_PATH = os.path.join(WEIGHTS_DIR, 'LSTM_w2v0_v20000_g10_w-007-tloss6.5609-vloss6.5036.hdf5')
 STEPS_PER_EPOCH = 10000
 VALIDATION_SPLIT = 0.2
 STEPS_VAL = 2000
@@ -141,10 +142,14 @@ LSTM_w2v0_v20000_g10_w-002-tloss6.0294-vloss5.7346
 
 [gvictorg@int2 src]$ squeue -u gvictorg
              JOBID PARTITION     NAME     USER ST       TIME  NODES NODELIST(REASON)
-           3257604       gpu      fff gvictorg  R   13:16:48      1 gcn28
-           3257603       gpu      fff gvictorg  R   13:17:06      1 gcn8
-           3256855       gpu yesrmsSm gvictorg  R   21:31:39      1 gcn11
-           3256854       gpu yesrmsSm gvictorg  R   21:32:52      1 gcn10
-           3256567       gpu       ff gvictorg  R 1-13:20:47      1 gcn14
+           3257604       gpu      fff gvictorg  R   13:16:48      1 gcn28   w17 
+           3257603       gpu      fff gvictorg  R   13:17:06      1 gcn8    w07
+           3256855       gpu yesrmsSm gvictorg  R   21:31:39      1 gcn11   w12 10:45
+           3256854       gpu yesrmsSm gvictorg  R   21:32:52      1 gcn10   w02
+           3256567       gpu       ff gvictorg  R 1-13:20:47      1 gcn14   w15 
 
+           3257604       gpu      fff gvictorg  R   23:10:51      1 gcn28
+           3257603       gpu      fff gvictorg  R   23:11:09      1 gcn8
+           3256855       gpu yesrmsSm gvictorg  R 1-07:25:42      1 gcn11
+           3256854       gpu yesrmsSm gvictorg  R 1-07:26:55      1 gcn10
 '''
